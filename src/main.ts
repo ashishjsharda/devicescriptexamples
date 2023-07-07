0d4ccc2f32c1d340a196ba576fdfbcef80e2fa62
Ashish Sharda <ashishjsharda@gmail.com>
2023-07-07 06:07:14 -0500

    add button events

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import * as ds from "@devicescript/core"
+import { debounceTime, filter } from "@devicescript/observables"
 
-setInterval(async () => {
-    console.log(":)")
-}, 1000)
+const sensor = new ds.AirPressure()
+const mouse = new ds.HidMouse()
+// listen for pressure changes
+sensor.reading
+    .pipe(
+        filter(pressure => pressure > 1400),
+        debounceTime(500)
+    )
+    .subscribe(async () => {
+        console.log(`click!`)
+        await mouse.setButton(
+            ds.HidMouseButton.Left,
+            ds.HidMouseButtonEvent.Click
+        )
+    })
